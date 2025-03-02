225cfb6422b96ab4f28305cd3d22bc06ad891c4d
Maciej Majka <magic_of_x@o2.pl>
2025-03-02 13:32:45 +0100

    init commit

---
+++ src/Pages/Stats.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/Components/ui/button"
+
+
+export default function Stats(){
+    return(<div id="statsPage">
+        <h1>Stats</h1>
+        <Button>Click me!</Button>
+    </div>
+    )
+}
